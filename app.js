11a3540ffe6315424d588d1d3d345c5299a8f8b7
ai-godson <godson.liu@anker-in.com>
2024-01-04 10:11:56 +0800

    update

--- app.js
+++ app.js
@@ -2,14 +2,71 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
+const feishu = process.env.FEISHU;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  res.end(JSON.stringify(req.body, null, 2));
+  res.end("Hello World!");
 });
 
-app.listen(port);
+app.post("/", async (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  const { name, title, commit_url } = req.body;
+  let url;
+  try {
+    url = JSON.parse(feishu);
+  } catch (e) {}
+
+  if (!url) {
+    res.end("feishu is not set");
+    return;
+  }
+
+  if (typeof url === "object") {
+    url = url[name];
+  }
+
+  if (!url) {
+    res.end("feishu is not match");
+    return;
+  }
+
+  await axios.post(url, {
+    msg_type: "interactive",
+    card: {
+      elements: [
+        {
+          tag: "div",
+          text: { content: title, tag: "lark_md" },
+        },
+        {
+          actions: [
+            {
+              tag: "button",
+              text: { content: "详情👀", tag: "lark_md" },
+              url: commit_url,
+              type: "default",
+              value: {},
+            },
+          ],
+          tag: "action",
+        },
+      ],
+      header: {
+        title: { content: "🚩 eufy | 代码发布提醒", tag: "plain_text" },
+      },
+    },
+  });
+
+  res.end("success");
+});
+
+const server = app.listen(port, () =>
+  console.log(`app listening on port ${port}!`)
+);
+
+// server.keepAliveTimeout = 120 * 1000;
+// server.headersTimeout = 120 * 1000;
